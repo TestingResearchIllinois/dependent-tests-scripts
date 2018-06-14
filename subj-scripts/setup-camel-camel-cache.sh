1c30dfb73e81b2e1bfb05a590b4bcb8faea28578
Reed Oei <oei.reed@gmail.com>
2018-06-14 07:54:57 -0700

    Added camel cache setup script.

---
+++ subj-scripts/setup-camel-camel-cache.sh
@@ -0,0 +1,15 @@
+export DT_ROOT=$(cd "$(dirname $BASH_SOURCE)/../.."; pwd)
+
+export DT_SUBJ_ROOT=$DT_ROOT/camel-old-8013229758c2ec46e00fe392f08f90a42f98af3e
+export DT_SUBJ=$DT_ROOT/camel-old-8013229758c2ec46e00fe392f08f90a42f98af3e/components/camel-cache/target
+export DT_SUBJ_SRC=$DT_ROOT/camel-old-8013229758c2ec46e00fe392f08f90a42f98af3e/components/camel-cache
+export DT_CLASS=$DT_ROOT/camel-old-8013229758c2ec46e00fe392f08f90a42f98af3e/components/camel-cache/target/classes
+export DT_TESTS=$DT_ROOT/camel-old-8013229758c2ec46e00fe392f08f90a42f98af3e/components/camel-cache/target/test-classes
+export NEW_DT_SUBJ_ROOT=$DT_ROOT/camel-new-ce4e54ed594dbea4859f105b9ea34f32aa892628
+export NEW_DT_SUBJ=$DT_ROOT/camel-new-ce4e54ed594dbea4859f105b9ea34f32aa892628/components/camel-cache/target
+export NEW_DT_SUBJ_SRC=$DT_ROOT/camel-new-ce4e54ed594dbea4859f105b9ea34f32aa892628/components/camel-cache
+export NEW_DT_CLASS=$DT_ROOT/camel-new-ce4e54ed594dbea4859f105b9ea34f32aa892628/components/camel-cache/target/classes
+export NEW_DT_TESTS=$DT_ROOT/camel-new-ce4e54ed594dbea4859f105b9ea34f32aa892628/components/camel-cache/target/test-classes
+export SUBJ_NAME=camel-camel-cache
+export SUBJ_NAME_FORMAL=camel-camel-cache
+. ../setup-vars.sh
