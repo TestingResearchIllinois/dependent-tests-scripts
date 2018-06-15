fa9b75cc90f1a86b5a3d3b0943f26fca9f8a7fbb
Reed Oei <oei.reed@gmail.com>
2018-06-15 13:42:31 -0700

    Fixed check-enhanced-results script.

--- precomputed-lifetime/check-enhanced-results.sh
+++ precomputed-lifetime/check-enhanced-results.sh
@@ -5,13 +5,20 @@
 source ../setup-vars.sh
 
 for dir in "$@"; do
-    # Generate enhanced results
-    java -cp $DT_TOOLS: edu.washington.cs.dt.impact.figure.generator.EnhancedResultsFigureGenerator -directory "$dir/prioritization-results" -outputDirectory "$dir" -allowNegatives
+    if [[ -d "$dir" ]]; then
+        # Generate enhanced results
+        echo "[INFO] Generating enhanced results for: $dir"
+        java -cp $DT_TOOLS: edu.washington.cs.dt.impact.figure.generator.EnhancedResultsFigureGenerator -directory "$dir/prioritization-results" -outputDirectory "$dir" -allowNegatives
+        java -cp $DT_TOOLS: edu.washington.cs.dt.impact.figure.generator.EnhancedResultsFigureGenerator -directory "$dir/selection-results" -outputDirectory "$dir" -allowNegatives
+        java -cp $DT_TOOLS: edu.washington.cs.dt.impact.figure.generator.EnhancedResultsFigureGenerator -directory "$dir/parallelization-results" -outputDirectory "$dir" -allowNegatives
 
-    # Looks for negative values. If we find any, that means the enhanced technique is worse.
-    # TODO: Maybe go on a per-technique basis.
-    if grep -q "-" enhanced-*-results.tex; then
-        echo "Found negative values for $dir"
+        # Looks for negative values. If we find any, that means the enhanced technique is worse.
+        # TODO: Maybe go on a per-technique basis.
+        for fname in $(find "$dir" -name "enhanced-*-orig-results.tex"); do
+            if grep -q "-" "$fname"; then
+                echo "Found negative values for $fname"
+            fi
+        done
     fi
 done
 
