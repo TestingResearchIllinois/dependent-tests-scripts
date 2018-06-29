ef5136f097d032f72b2e60e83c8d0615cbcbd6a3
Reed Oei <oei.reed@gmail.com>
2018-06-29 13:37:35 -0700

    Updated.

--- precomputed-lifetime/result-updater.sh
+++ precomputed-lifetime/result-updater.sh
@@ -17,7 +17,7 @@ for RESULTS_FOLDER in "$@"; do
 
     SETUP_SCRIPT="setup-$RESULTS_FOLDER.sh"
 
-    if [[ ! -e "$SETUP_SCRIPT" ]]; then
+    if [[ -e "$SETUP_SCRIPT" ]]; then
         # Generally the setup scripts are written so that they assume you are in a particular directory, so
         # make sure we match that.
         CURRENT=$(pwd)
@@ -34,22 +34,6 @@ for RESULTS_FOLDER in "$@"; do
     else
         echo "No setup script found with name $SETUP_SCRIPT!"
         exit 1
-    fi
-
-    echo "[INFO] Running subject ${SUBJ_NAME}"
-
-    RESULTS_DIR="$DT_SCRIPTS/${SUBJ_NAME}-results"
-
-    echo "[INFO] Checking for results directory at $RESULTS_DIR"
-    # Make the results directory if it doesn't exist (and copy the setup script if applicable)
-    if [[ ! -d "$RESULTS_DIR" ]]; then
-        echo "[INFO] Creating results directory"
-        mkdir "$RESULTS_DIR"
-
-        if [[ ! -z "$1" ]]; then
-            echo "[INFO] Copying setup script"
-            cp "$1" "$RESULTS_DIR"
-        fi
     fi
 
     java -cp $DT_TOOLS: edu.washington.cs.dt.impact.tools.ResultFileUpdater -cp "$DT_TOOLS:$NEW_DT_CLASS:$NEW_DT_TESTS:$NEW_DT_LIBS:$NEW_DT_RANDOOP" --orig-order "$NEW_DT_SUBJ/${SUBJ_NAME}-orig-order" --auto-order "$NEW_DT_SUBJ/${SUBJ_NAME}-auto-order" --result-dir "$RESULTS_FOLDER"
