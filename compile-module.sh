70e5043691eaea94c77b625168b398900f1987af
Reed Oei <oei.reed@gmail.com>
2018-08-06 14:28:35 -0700

    Minor improvements to scripts.

---
+++ compile-module.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Usage: bash compile-module.sh CLASSPATH MODULE_PATH [ROOT_PATH]
+# Classpath is a list of files (separated by ':') which should exist after compilation
+# MODULE_PATH is the path to compile from first, then will try ROOT_PATH (if exists and different)
+
+CLASSPATH="$1"
+MODULE_PATH="$2"
+ROOT_PATH="$3"
+
+if bash "$DT_SCRIPTS/check-compiled.sh" "$CLASSPATH"; then
+    echo "[INFO] Project already compiled, exiting."
+    exit 0
+fi
+
+# Try to compile just the module we need.
+(
+    cd "$MODULE_PATH"
+    mvn compile test-compile -Dmaven.javadoc.skip=true -DskipTests -Drat.skip=true -Dcobertura.skip
+
+    # 2. Gather the dependencies of the old subject.
+    mvn install -fn -Dmaven.javadoc.skip=true -DskipTests dependency:copy-dependencies -Drat.skip=true -Dcobertura.skip
+
+    bash "$DT_SCRIPTS/unsign-jars.sh" "$MODULE_PATH/target/dependency"
+) | tee "$DT_SCRIPTS/compile-output/${SUBJ_NAME}-module.txt"
+
+if grep -q "BUILD FAILURE" "$DT_SCRIPTS/compile-output/${SUBJ_NAME}-module.txt"; then
+    >&2 echo "[INFO] One or more builds failed. See ${SUBJ_NAME}-module.txt for more information. Trying to compile from root."
+
+    (
+        cd "$ROOT_PATH"
+        mvn compile test-compile -Dmaven.javadoc.skip=true -DskipTests -Drat.skip=true -Dcobertura.skip
+
+        # 2. Gather the dependencies of the old subject.
+        mvn install -fn -Dmaven.javadoc.skip=true -DskipTests dependency:copy-dependencies -Drat.skip=true -Dcobertura.skip
+
+        bash "$DT_SCRIPTS/unsign-jars.sh" "$MODULE_PATH/target/dependency"
+    ) | tee "$DT_SCRIPTS/compile-output/${SUBJ_NAME}-root.txt"
+
+    if grep -q "BUILD FAILURE" "$DT_SCRIPTS/compile-output/${SUBJ_NAME}-root.txt"; then
+        >&2 echo "[INFO] Compiling from root failed. See ${SUBJ_NAME}-root.txt for more information."
+        exit 1
+    fi
+fi
+
--- compile-subj.sh
+++ compile-subj.sh
@@ -1,59 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Automates the "Instructions for compiling subjects" section.
 
-# 1. Compile the subjects.
-
-if bash check-compiled.sh "$DT_CLASS:$DT_TESTS:$DT_LIBS:$NEW_DT_CLASS:$NEW_DT_TESTS:$NEW_DT_LIBS"; then
-    echo "[INFO] Project already compiled, exiting."
-    exit 0
-fi
-
-# Try to compile just the module we need.
-(
-    cd $DT_SUBJ_SRC
-    mvn compile test-compile -Dmaven.javadoc.skip=true -DskipTests -Drat.skip=true -Dcobertura.skip
-
-    # 2. Gather the dependencies of the old subject.
-    mvn install -fn -Dmaven.javadoc.skip=true -DskipTests dependency:copy-dependencies -Drat.skip=true -Dcobertura.skip
-
-    bash $DT_SCRIPTS/unsign-jars.sh $DT_SUBJ/dependency
-
-    # 3. Compile the new subject.
-    cd $NEW_DT_SUBJ_SRC
-    mvn compile test-compile -Dmaven.javadoc.skip=true -DskipTests -Drat.skip=true -Dcobertura.skip
-
-    # 4. Gather the dependencies of the new subject.
-    mvn install -fn -Dmaven.javadoc.skip=true -DskipTests dependency:copy-dependencies -Drat.skip=true -Dcobertura.skip
-
-    bash $DT_SCRIPTS/unsign-jars.sh $NEW_DT_SUBJ/dependency
-) | tee "$DT_SCRIPTS/compile-output/${SUBJ_NAME}-module.txt"
-
-if grep -q "BUILD FAILURE" "$DT_SCRIPTS/compile-output/${SUBJ_NAME}-module.txt"; then
-    >&2 echo "[INFO] One or more builds failed. See ${SUBJ_NAME}-module.txt for more information. Trying to compile from root."
-
-    (
-        cd $DT_SUBJ_ROOT
-        mvn compile test-compile -Dmaven.javadoc.skip=true -DskipTests -Drat.skip=true -Dcobertura.skip
-
-        # 2. Gather the dependencies of the old subject.
-        mvn install -fn -Dmaven.javadoc.skip=true -DskipTests dependency:copy-dependencies -Drat.skip=true -Dcobertura.skip
-
-        bash $DT_SCRIPTS/unsign-jars.sh $DT_SUBJ/dependency
-
-        # 3. Compile the new subject.
-        cd $NEW_DT_SUBJ_ROOT
-        mvn compile test-compile -Dmaven.javadoc.skip=true -DskipTests -Drat.skip=true -Dcobertura.skip
-
-        # 4. Gather the dependencies of the new subject.
-        mvn install -fn -Dmaven.javadoc.skip=true -DskipTests dependency:copy-dependencies -Drat.skip=true -Dcobertura.skip
-
-        bash $DT_SCRIPTS/unsign-jars.sh $NEW_DT_SUBJ/dependency
-    ) | tee "$DT_SCRIPTS/compile-output/${SUBJ_NAME}-root.txt"
-
-    if grep -q "BUILD FAILURE" "$DT_SCRIPTS/compile-output/${SUBJ_NAME}-root.txt"; then
-        >&2 echo "[INFO] Compiling from root failed. See ${SUBJ_NAME}-root.txt for more information."
-        exit 1
-    fi
-fi
+bash "$DT_SCRIPTS/compile-module.sh" "$DT_CLASS:$DT_TESTS:$DT_LIBS" "$DT_SUBJ_SRC" "$DT_SUBJ_ROOT"
+bash "$DT_SCRIPTS/compile-module.sh" "$NEW_DT_CLASS:$NEW_DT_TESTS:$NEW_DT_LIBS" "$NEW_DT_SUBJ_SRC" "$NEW_DT_SUBJ_ROOT"
 
