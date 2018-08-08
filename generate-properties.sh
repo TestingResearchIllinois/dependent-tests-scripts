6ead32cdda32841e79f8d5bbf1e993c3773996d7
Reed Oei <oei.reed@gmail.com>
2018-08-08 14:55:27 -0700

    Improved generate properties.

--- generate-properties.sh
+++ generate-properties.sh
@@ -15,5 +15,25 @@ cd $SCRIPT_DIR
 
 cd $CURRENT
 
-java -cp "$DT_TOOLS:" edu.washington.cs.dt.impact.tools.SubjectPropertiesGenerator
+DIR="$DT_SCRIPTS/$SUBJ_NAME-results/enhanced"
+
+go() {
+    java -cp $DT_TOOLS: edu.washington.cs.dt.impact.tools.GetAllUniqueDTs -prioDirectory $DIR/$1 -paraDirectory temp/ -seleDirectory temp/ -minBoundOrigDTFile orig -minBoundAutoDTFile auto > /dev/null
+    origNum=$(cat orig | head -1 | cut -f2 -d"|")
+    autoNum=$(cat auto | head -1 | cut -f2 -d"|")
+
+    echo "subject.$2.orig.dts=$origNum"
+    echo "subject.$2.auto.dts=$autoNum"
+
+    > orig 
+    > auto
+}
+
+(
+    echo "subject.name=$SUBJ_NAME"
+    echo "subject.formal_name=$SUBJ_NAME_FORMAL"
+    go prioritization-results prio
+    go selection-results sele
+    go parallelization-results para
+) | tee "$DIR/../subject.properties"
 
