5bf397e02944c2f79ccba5a220dd7432fc89b57a
August <awshi2@illinois.edu>
2019-04-18 17:31:24 +0000

    Changing parameter list

--- precomputed-lifetime/run-precomputed-lifetime.sh
+++ precomputed-lifetime/run-precomputed-lifetime.sh
@@ -1,23 +1,23 @@
 # Inputs:
 # $1 - project slug
 # $2 - path in repo (probably for module)
-# $3 - techniques to use (e.g., prio-sele-para or prio-para or sele-prio). Optional. If not provided, then will use all by default
-# $4 - path to file with commits for the project/module
+# $3 - path to file with commits for the project/module
+# $4 - techniques to use (e.g., prio-sele-para or prio-para or sele-prio). Optional. If not provided, then will use all by default
 
 if [[ "$1" == "--help" ]]; then
-    echo "Usage: ./run-precomputed-lifetime.sh project-slug relative-module-path [techniques] commit-list"
+    echo "Usage: ./run-precomputed-lifetime.sh project-slug relative-module-path commit-list [techniques]"
     exit 1
 fi
 
 if [[ "$#" -lt 1 ]]; then
-    echo "Usage: ./run-precomputed-lifetime.sh project-slug relative-module-path [techniques] commit-list"
+    echo "Usage: ./run-precomputed-lifetime.sh project-slug relative-module-path commit-list [techniques]"
     exit 1
 fi
 
 PROJ_SLUG=$1
 MODULE_PATH=$2
-TECHNIQUES="$3"
-NEW_COMMIT_FILE="$4"
+NEW_COMMIT_FILE="$3"
+TECHNIQUES="$4"
 # OLD_COMMIT_FILE="old-commit-list.txt"
 
 PROJ_NAME=$(echo $PROJ_SLUG | grep -Eo "([^/]+)\$") # Detect the project name
