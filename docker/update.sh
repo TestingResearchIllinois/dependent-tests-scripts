6791a270f7c37600dc7736bbb0789552dc557a14
August Shi <awshi2@illinois.edu>
2019-04-19 00:01:41 -0500

    Making sure it installs dtdetector before building

--- docker/update.sh
+++ docker/update.sh
@@ -4,4 +4,5 @@
 
 cd /home/awshi2/testrunner/ && git pull && /home/awshi2/apache-maven/bin/mvn clean install -B
 cd /home/awshi2/dependent-tests-scripts/ && git pull
+cd /home/awshi2/dependent-tests-scripts/ && mvn install:install-file -Dfile=impact-tools/dtdetector-1.2.0.jar -DgroupId=edu.washington.cs.dt -DartifactId=dtdetector -Dversion=1.2.0 -Dpackaging=jar
 cd /home/awshi2/dependent-tests-impact/ && git pull && /home/awshi2/apache-maven/bin/mvn clean install -DskipTests -B
