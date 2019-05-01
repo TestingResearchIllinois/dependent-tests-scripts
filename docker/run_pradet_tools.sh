03da4774739702ddf5a20c50e34d4b6baf70b4de
August Shi <awshi2@illinois.edu>
2019-05-01 13:11:58 -0500

    Modifying setup for pradet runs

--- docker/run_pradet_tools.sh
+++ docker/run_pradet_tools.sh
@@ -6,21 +6,23 @@ echo "Starting run_pradet_tools.sh"
 # This script is run inside the Docker image, for single experiment (one project)
 # Should only be invoked by the run_experiment.sh script
 
-if [[ $1 == "" ]] || [[ $2 == "" ]] || [[ $3 == "" ]] || [[ $4 == "" ]]; then
+if [[ $1 == "" ]] || [[ $2 == "" ]] || [[ $3 == "" ]] || [[ $4 == "" ]] || [[ $5 == "" ]]; then
     echo "arg1 - GitHub SLUG"
-    echo "arg2 - Old commit SHA/HEAD"
-    echo "arg3 - New commit SHA/HEAD"
-    echo "arg4 - Timeout in seconds"
-    echo "arg5 - (Optional) test type"
-    echo "arg6 - (Optional) time to leave docker image open. Largely for debugging."
+    echo "arg2 - Module"
+    echo "arg3 - Old commit SHA/HEAD"
+    echo "arg4 - New commit SHA/HEAD"
+    echo "arg5 - Timeout in seconds"
+    echo "arg6 - (Optional) test type"
+    echo "arg7 - (Optional) time to leave docker image open. Largely for debugging."
     exit
 fi
 
 slug=$1
-oldcommit=$2
-newcommit=$3
-timeout=$4
-testtype=$5
+module=$2
+oldcommit=$3
+newcommit=$4
+timeout=$5
+testtype=$6
 
 keepimagetime=$6
 if [[ -z "$6" ]]; then
@@ -46,6 +48,12 @@ cd pradet-replication/
 cd /home/awshi2/dependent-tests-scripts/
 
 set -x
+
+# Modify the modules-torun.txt to only include the relevant one for the specified module
+modifiedslug=${slug//\//.}
+grep "${modifiedslug}-$(basename ${module})" modules-torun.txt > /tmp/mod
+mv /tmp/mod modules-torun.txt
+
 timeout ${timeout}s /home/awshi2/dependent-tests-scripts/run-project-w-dir.sh ${slug} ${newcommit} ${oldcommit} "pradet"
 
 # Gather the results, put them up top
