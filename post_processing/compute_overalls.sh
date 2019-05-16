6fbb2ae2b392faca2b4c1edb03c27e557ae16f30
August Shi <awshi2@illinois.edu>
2019-05-16 14:23:40 -0500

    Making path to directory with data macros a parameter

--- post_processing/compute_overalls.sh
+++ post_processing/compute_overalls.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
+TEXDATADIR=$1
+if [[ ${TEXDATADIR} == "" ]]; then
+    echo "arg1 - Path to data directory in paper with all the results macros"
+    exit
+fi
+
 # LOC
-echo "\Def{overall_srcloc}{$(grep "_srcloc" data/loc.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)}"
-echo "\Def{overall_testloc}{$(grep "_testloc" data/loc.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)}"
+echo "\Def{overall_srcloc}{$(grep "_srcloc" ${TEXDATADIR}/loc.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)}"
+echo "\Def{overall_testloc}{$(grep "_testloc" ${TEXDATADIR}/loc.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)}"
 
 # Tests
-origtests=$(grep "_orig_numtests" data/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
+origtests=$(grep "_orig_numtests" ${TEXDATADIR}/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
 echo "\Def{overall_orig_numtests}{${origtests}}"
-autotests=$(grep "_auto_numtests" data/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
+autotests=$(grep "_auto_numtests" ${TEXDATADIR}/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
 echo "\Def{overall_auto_numtests}{${autotests}}"
 
 # DTs
-origdts=$(grep "_orig_dts}" data/dts.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
+origdts=$(grep "_orig_dts}" ${TEXDATADIR}/dts.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
 echo "\Def{overall_orig_dts}{${origdts}}"
 echo "\Def{overall_orig_dtsperc}{$(echo "${origdts} / ${origtests} * 100" | bc -l | xargs printf "%.0f")\%}"
 
-autodts=$(grep "_auto_dts}" data/dts.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
+autodts=$(grep "_auto_dts}" ${TEXDATADIR}/dts.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
 echo "\Def{overall_auto_dts}{${autodts}}"
 echo "\Def{overall_auto_dtsperc}{$(echo "${autodts} / ${autotests} * 100" | bc -l | xargs printf "%.0f")\%}"
 
 # Revisions
-echo "\Def{overall_orig_numrevs}{$(grep "_orig_numrevs}" data/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)}"
+echo "\Def{overall_orig_numrevs}{$(grep "_orig_numrevs}" ${TEXDATADIR}/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)}"
 
 # Compute overall percentage dts per technique
 for tech in prio sele para; do
     for testtype in orig auto; do
-        techres=$(grep "_${testtype}_dts_${tech}}" data/dts.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
+        techres=$(grep "_${testtype}_dts_${tech}}" ${TEXDATADIR}/dts.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
         if [[ ${testtype} == orig ]]; then
             total=${origdts}
         else
@@ -48,7 +54,7 @@ function breakdowns() {
     for cov in func stat; do
         for type in abso rela; do
             macrokey=${testtype}_dts_${tech}_${cov}_${type}
-            echo "\Def{${macrokey}_perc}{$(echo "$(echo "$(grep ${macrokey} data/breakdown.tex | cut -d'{' -f3 | cut -d'}' -f1) / ${total} * 100" | bc -l | xargs printf "%.0f")")\%}"
+            echo "\Def{${macrokey}_perc}{$(echo "$(echo "$(grep ${macrokey} ${TEXDATADIR}/breakdown.tex | cut -d'{' -f3 | cut -d'}' -f1) / ${total} * 100" | bc -l | xargs printf "%.0f")")\%}"
         done
     done
 
@@ -56,14 +62,14 @@ function breakdowns() {
     for cov in func stat; do
         for type in orig abso rela; do
             macrokey=${testtype}_dts_${tech}_${cov}_${type}
-            echo "\Def{${macrokey}_perc}{$(echo "$(echo "$(grep ${macrokey} data/breakdown.tex | cut -d'{' -f3 | cut -d'}' -f1) / ${total} * 100" | bc -l | xargs printf "%.0f")")\%}"
+            echo "\Def{${macrokey}_perc}{$(echo "$(echo "$(grep ${macrokey} ${TEXDATADIR}/breakdown.tex | cut -d'{' -f3 | cut -d'}' -f1) / ${total} * 100" | bc -l | xargs printf "%.0f")")\%}"
         done
     done
 
     tech=para
     for type in orig time; do
         macrokey=${testtype}_dts_${tech}_${type}
-        echo "\Def{${macrokey}_perc}{$(echo "$(echo "$(grep ${macrokey} data/breakdown.tex | cut -d'{' -f3 | cut -d'}' -f1) / ${total} * 100" | bc -l | xargs printf "%.0f")")\%}"
+        echo "\Def{${macrokey}_perc}{$(echo "$(echo "$(grep ${macrokey} ${TEXDATADIR}/breakdown.tex | cut -d'{' -f3 | cut -d'}' -f1) / ${total} * 100" | bc -l | xargs printf "%.0f")")\%}"
     done
 }
 
@@ -74,8 +80,8 @@ breakdowns auto ${autodts}
 for proj in $(cat projects.txt); do
     for testtype in orig auto; do
         for tech in prio sele para; do
-            techres=$(grep "${proj}_${testtype}_dts_${tech}}" data/dts.tex | cut -d'{' -f3 | cut -d'}' -f1)
-            giventechres=$(grep "${proj}_${testtype}_dtsgiven_${tech}}" data/givenresults.tex | cut -d'{' -f3 | cut -d'}' -f1)
+            techres=$(grep "${proj}_${testtype}_dts_${tech}}" ${TEXDATADIR}/dts.tex | cut -d'{' -f3 | cut -d'}' -f1)
+            giventechres=$(grep "${proj}_${testtype}_dtsgiven_${tech}}" ${TEXDATADIR}/givenresults.tex | cut -d'{' -f3 | cut -d'}' -f1)
             if [[ ${techres} == 0 ]]; then
                 echo "\Def{${proj}_${testtype}_dtsdiff_${tech}}{-}"
             else
@@ -95,11 +101,11 @@ for testtype in orig auto; do
         totaldtfailures=0
         totaldtfailuresgiven=0
         for proj in $(cat projects.txt); do
-            techres=$(grep "${proj}_${testtype}_dtfailures_${tech}}" data/results.tex | cut -d'{' -f3 | cut -d'}' -f1)
-            giventechres=$(grep "${proj}_${testtype}_dtfailuresgiven_${tech}}" data/givenresults.tex | cut -d'{' -f3 | cut -d'}' -f1)
+            techres=$(grep "${proj}_${testtype}_dtfailures_${tech}}" ${TEXDATADIR}/results.tex | cut -d'{' -f3 | cut -d'}' -f1)
+            giventechres=$(grep "${proj}_${testtype}_dtfailuresgiven_${tech}}" ${TEXDATADIR}/givenresults.tex | cut -d'{' -f3 | cut -d'}' -f1)
             if [[ ${techres} == 0 ]]; then
                 # Check if there actually are dts for this project
-                if [[ $(grep "${proj}_${testtype}_dts}" data/dts.tex| cut -d'{' -f3 | cut -d'}' -f1) != 0 ]]; then
+                if [[ $(grep "${proj}_${testtype}_dts}" ${TEXDATADIR}/dts.tex| cut -d'{' -f3 | cut -d'}' -f1) != 0 ]]; then
                     # In case of the weird case where more failures due to given, but is still 0
                     if [[ ${giventechres} != 0 ]]; then
                         echo "\Def{${proj}_${testtype}_dtfailuresdiff_${tech}}{-100\%*}"
@@ -134,21 +140,21 @@ for testtype in orig auto; do
         totaltimesgiven=0
         for proj in $(cat projects.txt); do
             # For para, need to observe if the technique detects any DTs, and if not then should just skip
-            if [[ ${tech} == "para" && $(grep "${proj}_${testtype}_dts_${tech}}" data/dts.tex | cut -d'{' -f3 | cut -d'}' -f1) == 0 ]]; then
+            if [[ ${tech} == "para" && $(grep "${proj}_${testtype}_dts_${tech}}" ${TEXDATADIR}/dts.tex | cut -d'{' -f3 | cut -d'}' -f1) == 0 ]]; then
                 echo "\Def{${proj}_${testtype}_testtimediff_${tech}}{n/e}"
                 continue
             fi
             # For sele, need to observe if either para or prio of it had any DTs, and skip if both are 0
             if [[ ${tech} == "sele" ]]; then
-                para=$(grep "${proj}_${testtype}_dts_para}" data/dts.tex | cut -d'{' -f3 | cut -d'}' -f1)
-                prio=$(grep "${proj}_${testtype}_dts_prio}" data/dts.tex | cut -d'{' -f3 | cut -d'}' -f1)
+                para=$(grep "${proj}_${testtype}_dts_para}" ${TEXDATADIR}/dts.tex | cut -d'{' -f3 | cut -d'}' -f1)
+                prio=$(grep "${proj}_${testtype}_dts_prio}" ${TEXDATADIR}/dts.tex | cut -d'{' -f3 | cut -d'}' -f1)
                 if [[ ${para} == 0 && ${prio} == 0 ]]; then
                     echo "\Def{${proj}_${testtype}_testtimediff_${tech}}{n/e}"
                     continue
                 fi
             fi
-            techres=$(grep "${proj}_${testtype}_testtime_${tech}}" data/times.tex | cut -d'{' -f3 | cut -d'}' -f1)
-            giventechres=$(grep "${proj}_${testtype}_testtimegiven_${tech}}" data/times.tex | cut -d'{' -f3 | cut -d'}' -f1)
+            techres=$(grep "${proj}_${testtype}_testtime_${tech}}" ${TEXDATADIR}/times.tex | cut -d'{' -f3 | cut -d'}' -f1)
+            giventechres=$(grep "${proj}_${testtype}_testtimegiven_${tech}}" ${TEXDATADIR}/times.tex | cut -d'{' -f3 | cut -d'}' -f1)
             if [[ ${techres} == 0 ]]; then
                 echo "\Def{${proj}_${testtype}_testtimediff_${tech}}{-}"
             else
@@ -174,22 +180,22 @@ for testtype in orig auto; do
         for proj in $(cat projects.txt); do
             # Need to observe if the technique detects any DTs, and if not then should just skip
             if [[ ${tech} == "prio" || ${tech} == "para" ]]; then
-                if [[ $(grep "${proj}_${testtype}_dts_${tech}}" data/dts.tex | cut -d'{' -f3 | cut -d'}' -f1) == 0 ]]; then
+                if [[ $(grep "${proj}_${testtype}_dts_${tech}}" ${TEXDATADIR}/dts.tex | cut -d'{' -f3 | cut -d'}' -f1) == 0 ]]; then
                     echo "\Def{${proj}_${testtype}_testtimererunsdiff_${tech}}{n/e}"
                     continue
                 fi
             fi
             # For sele, need to observe if either para or prio of it had any DTs, and skip if both are 0
             if [[ ${tech} == "sele" ]]; then
-                para=$(grep "${proj}_${testtype}_dts_para}" data/dts.tex | cut -d'{' -f3 | cut -d'}' -f1)
-                prio=$(grep "${proj}_${testtype}_dts_prio}" data/dts.tex | cut -d'{' -f3 | cut -d'}' -f1)
+                para=$(grep "${proj}_${testtype}_dts_para}" ${TEXDATADIR}/dts.tex | cut -d'{' -f3 | cut -d'}' -f1)
+                prio=$(grep "${proj}_${testtype}_dts_prio}" ${TEXDATADIR}/dts.tex | cut -d'{' -f3 | cut -d'}' -f1)
                 if [[ ${para} == 0 && ${prio} == 0 ]]; then
                     echo "\Def{${proj}_${testtype}_testtimererunsdiff_${tech}}{n/e}"
                     continue
                 fi
             fi
-            techres=$(grep "${proj}_${testtype}_testtimereruns_${tech}}" data/execsubsuitetimes.tex | cut -d'{' -f3 | cut -d'}' -f1)
-            giventechres=$(grep "${proj}_${testtype}_testtimererunsgiven_${tech}}" data/execsubsuitetimes.tex | cut -d'{' -f3 | cut -d'}' -f1)
+            techres=$(grep "${proj}_${testtype}_testtimereruns_${tech}}" ${TEXDATADIR}/execsubsuitetimes.tex | cut -d'{' -f3 | cut -d'}' -f1)
+            giventechres=$(grep "${proj}_${testtype}_testtimererunsgiven_${tech}}" ${TEXDATADIR}/execsubsuitetimes.tex | cut -d'{' -f3 | cut -d'}' -f1)
             if [[ ${techres} == 0 ]]; then
                 echo "\Def{${proj}_${testtype}_testtimererunsdiff_${tech}}{-}"
             else
@@ -206,19 +212,19 @@ done
 echo "\Def{overall_testtimererunsdiff}{$(echo "((${supertotaltimes} - ${supertotaltimesgiven}) * 100) / ${supertotaltimes}" | bc -l | xargs printf "%.0f")\%}"
 
 # Compute some overalls concerning configurations with dts
-totalconfs=$(grep "_totalconfs_" data/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
-confswithdts=$(grep "_confswithdts_" data/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
+totalconfs=$(grep "_totalconfs_" ${TEXDATADIR}/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
+confswithdts=$(grep "_confswithdts_" ${TEXDATADIR}/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
 echo "\Def{overall_confswithdtsperc}{$(echo "${confswithdts} / ${totalconfs} * 100" | bc -l | xargs printf "%.0f")\%}"
 
-totalconfsgiven=$(grep "_totalconfsgiven_" data/givenresults.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
-confswithdtsgiven=$(grep "_confswithdtsgiven_" data/givenresults.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
+totalconfsgiven=$(grep "_totalconfsgiven_" ${TEXDATADIR}/givenresults.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
+confswithdtsgiven=$(grep "_confswithdtsgiven_" ${TEXDATADIR}/givenresults.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
 echo "\Def{overall_confswithdtsgivenperc}{$(echo "${confswithdtsgiven} / ${totalconfsgiven} * 100" | bc -l | xargs printf "%.0f")\%}"
 
 # Compute the same concerning configurations but per test type
-totalconfs=$(grep "_orig_totalconfs_" data/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
-confswithdts=$(grep "_orig_confswithdts_" data/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
+totalconfs=$(grep "_orig_totalconfs_" ${TEXDATADIR}/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
+confswithdts=$(grep "_orig_confswithdts_" ${TEXDATADIR}/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
 echo "\Def{overall_orig_confswithdtsperc}{$(echo "${confswithdts} / ${totalconfs} * 100" | bc -l | xargs printf "%.0f")\%}"
 
-totalconfs=$(grep "_auto_totalconfs_" data/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
-confswithdts=$(grep "_orig_confswithdts_" data/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
+totalconfs=$(grep "_auto_totalconfs_" ${TEXDATADIR}/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
+confswithdts=$(grep "_orig_confswithdts_" ${TEXDATADIR}/results.tex | cut -d'{' -f3 | cut -d'}' -f1 | paste -sd+ | bc -l)
 echo "\Def{overall_auto_confswithdtsperc}{$(echo "${confswithdts} / ${totalconfs} * 100" | bc -l | xargs printf "%.0f")\%}"
