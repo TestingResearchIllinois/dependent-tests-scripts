d7d56d3733252032938ea3e9949937e187f4c569
August Shi <awshi2@illinois.edu>
2020-05-20 02:30:58 -0500

    Including copied/hacked in scripts for quickly running with pradet deps and both deps

---
+++ docker/run_lifetime_tools_pradet.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+echo "*******************LIFETIME************************"
+echo "Starting run_lifetime_tools.sh"
+
+# This script is run inside the Docker image, for single experiment (one project)
+# Should only be invoked by the run_project_lifetime.sh script
+
+if [[ $1 == "" ]] || [[ $2 == "" ]] || [[ $3 == "" ]] || [[ $4 == "" ]] || [[ $5 == "" ]] || [[ $6 = "" ]]; then
+    echo "arg1 - GitHub SLUG"
+    echo "arg2 - Module"
+    echo "arg3 - Old commit SHA/HEAD"
+    echo "arg4 - New commit SHA/HEAD"
+    echo "arg5 - Timeout in seconds"
+    echo "arg6 - Test type"
+    echo "arg7 - Technique (Optional)"
+    exit
+fi
+
+slug=$1
+module=$2
+oldcommit=$3
+newcommit=$4
+timeout=$5
+testtype=$6
+technique=$7
+
+# Run the plugin, get module test times
+echo "*******************ACCOMMODATER************************"
+echo "Running the lifetime tools for multiple commits"
+cd /home/awshi2/${slug}/
+git rev-parse HEAD
+date
+
+mkdir -p /home/awshi2/data
+
+cd /home/awshi2/dependent-tests-scripts/
+
+set -x
+
+modifiedslug=${slug//\//.}
+
+# Modify the modules-torun.txt to only include the relevant one for the specified module
+grep "${modifiedslug}-$(basename ${module})" modules-torun.txt > /tmp/mod
+mv /tmp/mod modules-torun.txt
+
+# Go to the lifetime directory and start running 
+cd precomputed-lifetime/
+
+# Download the directories corresponding to the old SHA and put them into place
+(
+    cd /home/awshi2/
+    if [[ ${module} == '.' ]]; then
+        name=${modifiedslug}
+    else
+        name=${modifiedslug}-$(basename ${module})
+    fi
+    wget http://mir.cs.illinois.edu/awshi2/dt-impact/${name}_pradet.zip
+    unzip ${name}_pradet.zip
+    owner=$(echo ${slug} | cut -d'/' -f1)
+    projname=$(echo ${slug} | cut -d'/' -f2)
+    mv ${name}/${projname}-old-* /home/awshi2/${owner}/
+    mkdir -p /home/awshi2/data/
+    mv ${name}/${name}-results/dt-lists/* /home/awshi2/data/
+    mv ${name}/${name}-results/ /home/awshi2/dependent-tests-scripts/
+)
+
+# Actually run the script
+timeout ${timeout}s bash /home/awshi2/dependent-tests-scripts/precomputed-lifetime/run-precomputed-lifetime.sh ${slug} ${module} /home/awshi2/dependent-tests-scripts/precomputed-lifetime/projectcommits/${modifiedslug}-$(basename ${module})_commits ${testtype} ${technique}
+
+# In case of timeout (or other errors...), put the other stuff into the results
+if [[ $? != 0 ]]; then
+    mkdir timeout-results/
+    mv /home/awshi2/data/ timeout-results/
+    mv /home/awshi2/prioritization-results/ timeout-results/
+    mv /home/awshi2/parallelization-results/ timeout-results/
+    mv /home/awshi2/selection-results/ timeout-results/
+fi
+
+# Gather the results, put them up top
+cd /home/awshi2/dependent-tests-scripts/precomputed-lifetime/
+RESULTSDIR=/home/awshi2/output/
+mkdir -p ${RESULTSDIR}
+for d in $(find -maxdepth 1 -type d -name "*-lifetime"); do mv $d ${RESULTSDIR}/; done
+for d in $(find -maxdepth 1 -type d -name "*-results"); do mv $d ${RESULTSDIR}/; done
+
+echo "*******************ACCOMMODATER************************"
+echo "Finished run_lifetime_tools.sh"
+date
+
