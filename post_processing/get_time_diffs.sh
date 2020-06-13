9ffb094e895dcd055abe89c7e461dc897f6e4db8
August Shi <awshi2@illinois.edu>
2020-06-12 22:57:02 -0500

    New ways to compute time diffs for parallelization

--- post_processing/get_time_diffs.sh
+++ post_processing/get_time_diffs.sh
@@ -13,6 +13,7 @@ function compute_selection() {
     # Rolling sums for the final overall number
     rollingomittedtotaltime=0
     rollinggiventotaltime=0
+    rollingfound=0
 
     for p in $(ls | grep "_output$"); do
         projmod=$(echo ${p} | sed 's;_output;;')
@@ -20,6 +21,7 @@ function compute_selection() {
         # Sum up the times between the two, omitted and given
         omittedtotaltime=0
         giventotaltime=0
+        found=0
 
         # Match up the OMITTED with the GIVEN to check if there is a difference and if should count
         for omitted in $(find ${p} -name SELECTION-* | grep OMITTED | grep "true"); do
@@ -32,6 +34,8 @@ function compute_selection() {
             if [[ $(diff <(echo ${omittedtestorder}) <(echo ${giventestorder})) == "" ]]; then
                 continue
             fi
+            found=1
+            rollingfound=1
 
             # Otherwise, get the test time
             omittedtesttime=$(grep "Execution time for executing the following testing order: " ${omitted} | cut -d' ' -f9)
@@ -45,8 +49,12 @@ function compute_selection() {
         # Report the times and the diff between the two by percentage, using omittedtotaltime as the baseline
         echo "\Def{${projmod}_${testtype}_testtime_sele}{${omittedtotaltime}}"
         echo "\Def{${projmod}_${testtype}_testtimegiven_sele}{${giventotaltime}}"
-        if [[ ${omittedtotaltime} != 0 ]]; then
-            echo "\Def{${projmod}_${testtype}_testtimediff_sele}{$(echo "(${giventotaltime} - ${omittedtotaltime}) / ${omittedtotaltime} * 100" | bc -l | xargs printf "%.0f")\%}"
+        if [[ ${found} == 1 ]]; then
+            if [[ ${omittedtotaltime} != 0 ]]; then
+                echo "\Def{${projmod}_${testtype}_testtimediff_sele}{$(echo "(${giventotaltime} - ${omittedtotaltime}) / ${omittedtotaltime} * 100" | bc -l | xargs printf "%.0f")\%}"
+            else
+                echo "\Def{${projmod}_${testtype}_testtimediff_sele}{0\%}"
+            fi
         else
             echo "\Def{${projmod}_${testtype}_testtimediff_sele}{=}"
         fi
@@ -56,15 +64,19 @@ function compute_selection() {
     done
 
     # Compute the overall summary across all projects
-    if [[ ${rollingomittedtotaltime} != 0 ]]; then
-        echo "\Def{overall_${testtype}_testtimediff_sele}{$(echo "(${rollinggiventotaltime} - ${rollingomittedtotaltime}) / ${rollingomittedtotaltime} * 100" | bc -l | xargs printf "%.0f")\%}"
+    if [[ ${rollingfound} == 1 ]]; then
+        if [[ ${rollingomittedtotaltime} != 0 ]]; then
+            echo "\Def{overall_${testtype}_testtimediff_sele}{$(echo "(${rollinggiventotaltime} - ${rollingomittedtotaltime}) / ${rollingomittedtotaltime} * 100" | bc -l | xargs printf "%.0f")\%}"
+        else
+            echo "\Def{overall_${testtype}_testtimediff_sele}{0\%}"
+        fi
     else
         echo "\Def{overall_${testtype}_testtimediff_sele}{=}"
     fi
 
 }
 
-function compute_parallelization() {
+function compute_parallelization_longest() {
 
     testtype=$1
 
@@ -86,7 +98,7 @@ function compute_parallelization() {
         # Match up the OMITTED with the GIVEN to check if there is a difference and if should count
         for omitted in $(find ${p} -name PARALLELIZATION-* | grep OMITTED | grep "true"); do
             # Only keep track of immediate next revision for parallelization
-            if [[ $(echo ${f} | grep "${firstsha}") == "" ]]; then
+            if [[ $(echo ${omitted} | grep "${firstsha}") == "" ]]; then
                 continue
             fi
 
@@ -153,7 +165,7 @@ function compute_parallelization() {
         echo "\Def{${projmod}_${testtype}_testtime_para}{${omittedtotaltime}}"
         echo "\Def{${projmod}_${testtype}_testtimegiven_para}{${giventotaltime}}"
         if [[ ${omittedtotaltime} != 0 ]]; then
-            echo "\Def{${projmod}_${testtype}_testtimediff_para}{$(echo "(${giventotaltime} - ${omittedtotaltime}) / ${omittedtotaltime} * 100" | bc -l | xargs printf "%.0f")}"
+            echo "\Def{${projmod}_${testtype}_testtimediff_para}{$(echo "(${giventotaltime} - ${omittedtotaltime}) / ${omittedtotaltime} * 100" | bc -l | xargs printf "%.0f")\%}"
         else
             echo "\Def{${projmod}_${testtype}_testtimediff_para}{=}"
         fi
@@ -164,7 +176,144 @@ function compute_parallelization() {
 
     # Compute the overall summary across all projects
     if [[ ${rollingomittedtotaltime} != 0 ]]; then
-        echo "\Def{overall_${testtype}_testtimediff_para}{$(echo "(${rollinggiventotaltime} - ${rollingomittedtotaltime}) / ${rollingomittedtotaltime} * 100" | bc -l | xargs printf "%.0f")}"
+        echo "\Def{overall_${testtype}_testtimediff_para}{$(echo "(${rollinggiventotaltime} - ${rollingomittedtotaltime}) / ${rollingomittedtotaltime} * 100" | bc -l | xargs printf "%.0f")\%}"
+    else
+        echo "\Def{overall_${testtype}_testtimediff_para}{=}"
+    fi
+}
+
+# Compute differences but not by matching up machine; simply match up the longest-running machine in both orders
+function compute_parallelization_longest_no_match() {
+
+    testtype=$1
+
+    # Rolling sums for the final overall number
+    rollingomittedtotaltime=0
+    rollinggiventotaltime=0
+
+    for p in $(ls | grep "_output$"); do
+        projmod=$(echo ${p} | sed 's;_output;;')
+
+        # Get the first revision
+        firstrev=$(find ${p} -name "*-lifetime" | xargs ls | sort | head -1)
+        firstsha=$(echo ${firstrev} | rev | cut -d'-' -f1 | rev | cut -c 1-8)
+
+        # Sum up the times between the two, omitted and given
+        omittedtotaltime=0
+        giventotaltime=0
+
+        # Match up the OMITTED with the GIVEN to check if there is a difference and if should count
+        for omitted in $(find ${p} -name PARALLELIZATION-* | grep OMITTED | grep "true"); do
+            # Only keep track of immediate next revision for parallelization
+            if [[ $(echo ${omitted} | grep "${firstsha}") == "" ]]; then
+                continue
+            fi
+
+            given=$(echo ${omitted} | sed 's;OMITTED;GIVEN;')
+
+            # Want to just get times corresponding to longest
+            longestomittedtime=0
+            longestgiventime=0
+
+            IFS=$'\n'
+            # Search for the longest omitted order time
+            for ot in $(grep "Execution time for executing the following testing order: " ${omitted}); do
+                omittedtesttime=$(echo ${ot} | cut -d' ' -f9)
+                # If the omitted time is greater than the longest time, we keep it
+                if [[ $(echo "${omittedtesttime} > ${longestomittedtime}" | bc -l) == 1 ]]; then
+                    longestomittedtime=${omittedtesttime}
+                fi
+            done
+            # Search for the longest given order time
+            for gt in $(grep "Execution time for executing the following testing order: " ${given}); do
+                giventesttime=$(echo ${gt} | cut -d' ' -f9)
+                # If the given time is greater than the longest time, we keep it
+                if [[ $(echo "${giventesttime} > ${longestgiventime}" | bc -l) == 1 ]]; then
+                    longestgiventime=${giventesttime}
+                fi
+            done
+
+            omittedtotaltime=$(echo ${omittedtotaltime} + ${longestomittedtime} | bc -l)
+            giventotaltime=$(echo ${giventotaltime} + ${longestgiventime} | bc -l)
+        done
+
+        # Report the times and the diff between the two by percentage, using omittedtotaltime as the baseline
+        echo "\Def{${projmod}_${testtype}_testtime_para}{${omittedtotaltime}}"
+        echo "\Def{${projmod}_${testtype}_testtimegiven_para}{${giventotaltime}}"
+        if [[ ${omittedtotaltime} != 0 ]]; then
+            echo "\Def{${projmod}_${testtype}_testtimediff_para}{$(echo "(${giventotaltime} - ${omittedtotaltime}) / ${omittedtotaltime} * 100" | bc -l | xargs printf "%.0f")\%}"
+        else
+            echo "\Def{${projmod}_${testtype}_testtimediff_para}{=}"
+        fi
+
+        rollingomittedtotaltime=$(echo "${rollingomittedtotaltime} + ${omittedtotaltime}" | bc -l)
+        rollinggiventotaltime=$(echo "${rollinggiventotaltime} + ${giventotaltime}" | bc -l)
+    done
+
+    # Compute the overall summary across all projects
+    if [[ ${rollingomittedtotaltime} != 0 ]]; then
+        echo "\Def{overall_${testtype}_testtimediff_para}{$(echo "(${rollinggiventotaltime} - ${rollingomittedtotaltime}) / ${rollingomittedtotaltime} * 100" | bc -l | xargs printf "%.0f")\%}"
+    else
+        echo "\Def{overall_${testtype}_testtimediff_para}{=}"
+    fi
+}
+
+function compute_parallelization() {
+
+    testtype=$1
+
+    # Rolling sums for the final overall number
+    rollingomittedtotaltime=0
+    rollinggiventotaltime=0
+
+    for p in $(ls | grep "_output$"); do
+        projmod=$(echo ${p} | sed 's;_output;;')
+
+        # Get the first revision
+        firstrev=$(find ${p} -name "*-lifetime" | xargs ls | sort | head -1)
+        firstsha=$(echo ${firstrev} | rev | cut -d'-' -f1 | rev | cut -c 1-8)
+
+        # Sum up the times between the two, omitted and given
+        omittedtotaltime=0
+        giventotaltime=0
+
+        # Compare the sum of all the sub test times in the files
+        for omitted in $(find ${p} -name PARALLELIZATION-* | grep OMITTED | grep "true"); do
+            # Only keep track of immediate next revision for parallelization
+            if [[ $(echo ${omitted} | grep "${firstsha}") == "" ]]; then
+                continue
+            fi
+
+            given=$(echo ${omitted} | sed 's;OMITTED;GIVEN;')
+
+            # Find all the test times
+            IFS=$'\n'
+            for ot in $(grep "Execution time for executing the following testing order: " ${omitted}); do
+                omittedtesttime=$(echo ${ot} | cut -d' ' -f9)
+                omittedtotaltime=$(echo ${omittedtotaltime} + ${omittedtesttime} | bc -l)
+            done
+            for gt in $(grep "Execution time for executing the following testing order: " ${given}); do
+                giventesttime=$(echo ${gt} | cut -d' ' -f9)
+                giventotaltime=$(echo ${giventotaltime} + ${giventesttime} | bc -l)
+            done
+        done
+
+        # Report the times and the diff between the two by percentage, using omittedtotaltime as the baseline
+        echo "\Def{${projmod}_${testtype}_testtime_para}{${omittedtotaltime}}"
+        echo "\Def{${projmod}_${testtype}_testtimegiven_para}{${giventotaltime}}"
+        if [[ ${omittedtotaltime} != 0 ]]; then
+            echo "\Def{${projmod}_${testtype}_testtimediff_para}{$(echo "(${giventotaltime} - ${omittedtotaltime}) / ${omittedtotaltime} * 100" | bc -l | xargs printf "%.0f")\%}"
+        else
+            echo "\Def{${projmod}_${testtype}_testtimediff_para}{=}"
+        fi
+
+        rollingomittedtotaltime=$(echo "${rollingomittedtotaltime} + ${omittedtotaltime}" | bc -l)
+        rollinggiventotaltime=$(echo "${rollinggiventotaltime} + ${giventotaltime}" | bc -l)
+    done
+
+    # Compute the overall summary across all projects
+    if [[ ${rollingomittedtotaltime} != 0 ]]; then
+        echo "\Def{overall_${testtype}_testtimediff_para}{$(echo "(${rollinggiventotaltime} - ${rollingomittedtotaltime}) / ${rollingomittedtotaltime} * 100" | bc -l | xargs printf "%.0f")\%}"
     else
         echo "\Def{overall_${testtype}_testtimediff_para}{=}"
     fi
@@ -180,6 +329,6 @@ for testtype in orig auto; do
     compute_selection ${testtype}
 
     # Parallelization
-    compute_parallelization ${testtype}
+    compute_parallelization_longest_no_match ${testtype}
 )
 done
