af18ca461bedc0a178678645ca1e90606197b17f
Wing Lam <winglam2@illinois.edu>
2020-06-19 03:56:06 +0000

    Finalizing example.

--- public/example.sh
+++ public/example.sh
@@ -3,38 +3,40 @@
 # Runs the enhanced T2 algorithm (prioritization, statement, relative) on kevinsawicki/http-request (M9).
 # Usage: bash example.sh
 
+scripts_folder=$(cd "$(dirname $BASH_SOURCE)"; pwd)
+
 # Clear any existing logs
-rm -rf logs/
-mkdir logs/
+rm -rf ${scripts_folder}/logs/
+mkdir -p ${scripts_folder}/logs/
 
 # Clone the firstVers if it doesn't exist
-if [ ! -d "firstVers" ]; then
+if [[ ! -d "firstVers" ]]; then
     echo "Cloning firstVers"
-    git clone https://github.com/kevinsawicki/http-request.git firstVers  > logs/firstVers-clone-log.txt
+    git clone https://github.com/kevinsawicki/http-request.git firstVers &> ${scripts_folder}/logs/firstVers-clone-log.txt
     echo "Compiling firstVers"
-    cd firstVers/lib/
-    git checkout d0ba95cf3c621c74a023887814e8c1f73b5da1b2
-    mvn install dependency:copy-dependencies -DskipTests -l logs/install-log-firstVers.txt
-    cd ../../
+    cd firstVers/lib
+    git checkout d0ba95cf3c621c74a023887814e8c1f73b5da1b2 &> ${scripts_folder}/logs/checkout-firstVers.txt
+    mvn install dependency:copy-dependencies -DskipTests &> ${scripts_folder}/logs/install-log-firstVers.txt
+    cd ${scripts_folder}
 fi
 
 # Clone the secondVers if it doesn't exist
-if [ ! -d "secondVers" ]; then
+if [[ ! -d "secondVers" ]]; then
     echo "Cloning secondVers"
-    git clone https://github.com/kevinsawicki/http-request.git secondVers > logs/secondVers-clone-log.txt
+    git clone https://github.com/kevinsawicki/http-request.git secondVers &> ${scripts_folder}/logs/secondVers-clone-log.txt
     echo "Compiling secondVers"
-    cd secondVers/lib/
-    git checkout ef89ec663e6d192c08b77dd1d9b8649975c1419c
-    mvn install dependency:copy-dependencies -DskipTests -l logs/install-log-secondVers.txt
-    cd ../../
+    cd secondVers/lib
+    git checkout ef89ec663e6d192c08b77dd1d9b8649975c1419c &> ${scripts_folder}/logs/checkout-secondVers.txt
+    mvn install dependency:copy-dependencies -DskipTests &> ${scripts_folder}/logs/install-log-secondVers.txt
+    cd ${scripts_folder}
 fi
 
 # Clear any existing results
 rm -rf lib-results/
 
 echo "Setting up the two versions for regression testing"
-bash setup.sh firstVers/lib t2 secondVers/lib > logs/setup.txt
+bash setup.sh firstVers/lib t2 secondVers/lib &> logs/setup.txt
 echo "Computing dependencies on the firstVers"
-bash compute-deps.sh firstVers/lib t2 secondVers/lib > logs/compute-deps.txt
+bash compute-deps.sh firstVers/lib t2 secondVers/lib &> logs/compute-deps.txt
 echo "Running the regression testing algorithm on the secondVers"
-bash run.sh firstVers/lib t2 secondVers/lib > logs/run.txt
+bash run.sh firstVers/lib t2 secondVers/lib &> logs/run.txt
